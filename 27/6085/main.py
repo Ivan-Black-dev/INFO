7c8a3177f61e0fcca48cc8e203eeb3e454d4aa9c
Ivan-Black-dev <ichernyy05@yandex.ru>
2023-03-07 08:36:55 +0300

    add true file A for 6085

--- 27/6085/main.py
+++ 27/6085/main.py
@@ -6,10 +6,18 @@ def ch3(n):
             n /= 3
         else:
             break
+    if c > 6:
+        return 6
     return c
 
+def ostat8(n):
+    if n % 8 == 0:
+        return 0
+    else:
+        return 8 - n % 8
 
-with open('27A.txt') as file:
+
+with open('B.txt') as file:
     f = [int(i) for i in file.readlines()]
 
 d = [[0 for i in range(0, 6+1)] for i in range(0, 8+1)]
@@ -17,18 +25,15 @@ f1 = f[15:]
 
 count = 0
 for i in range(len(f1)):
-    dy = ch3(f1[i])
-    dx = f1[i] % 8
+    dy = ch3(f[i])
+    dx = f[i] % 8
     d[dx][dy] += 1
 
-    n = f[i]
-    ost8 = n % 8
-    c3 = ch3(n)
-    y = ost8
-    if y == 8:
-        y = 0
-    x = c3
-    count += d[y][x]
+    n = f1[i]
+    y = ostat8(n)
+    x = 6 - ch3(n)
+    for x1 in range(x, 6):
+        count += d[y][x1]
 print(count)
 
 
